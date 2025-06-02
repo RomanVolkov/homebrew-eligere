e12e9adf21adeb1967f1f416e7ccd0d166c45e83
Roman Volkov <email@romanvolkov.ru>
2025-06-02 10:49:27 +0200

    v1.4.0

---
+++ Casks/eligere.rb
@@ -0,0 +1,16 @@
+cask "eligere" do
+  version "1.4.0_24"
+  sha256 "866ec98f483454d05070ac99c0d966d5b043e92190c5bc9f588377596a5a2a5f"
+
+  url "https://update.eligere.dev/Eligere_#{version}_Installer_compressed.dmg"
+  name "Eligere"
+  desc "Makes better links routing"
+  homepage "https://eligere.dev"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Eligere.app"
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Eligere Homebrew Tap
+
+## Installation
+
+```bash
+brew tap romanvolkov/eligere
+brew install --cask eligere
+```
