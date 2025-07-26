b7869e1b15a6bd1b5230a36b556a544257b22453
GitHub Action <action@github.com>
2025-07-26 00:14:56 +0000

    Update Eligere to version 1.4.6_30

--- Casks/eligere.rb
+++ Casks/eligere.rb
@@ -1,6 +1,6 @@
 cask "eligere" do
-  version "1.4.5_29"
-  sha256 "b498241977c7bc45471c417dc93652967f72b76402e6ad814d1b97a7af7ea255"
+  version "1.4.6_30"
+  sha256 "c2c71fe89325dc36f0148acb28905bd0ef6ea49270eaeb3873ac107b4bf31d16"
 
   url "https://update.eligere.dev/Eligere_#{version}_Installer_compressed.dmg"
   name "Eligere"
